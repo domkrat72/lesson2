86ebb4869b1013dbe6de4ef5d6c36f603eaebcc1
domkrat72 <snovikov72@gmail.com>
2024-10-02 19:14:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+number_of_homework_completed = 12
+number_of_hours_spent = 1.5
+average_execution_time= 0.125
+name = ('Piton')
+print(name, number_of_homework_completed, number_of_hours_spent, average_execution_time)
